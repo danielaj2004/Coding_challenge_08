c59b9c623d1281d8b4482a322cea5bd043f773ff
danielaj2004 <danielajoya12@gmail.com>
2025-02-16 22:41:43 -0500

    Task 2 - Product Price After Discount

--- cc_8.js
+++ cc_8.js
@@ -5,3 +5,11 @@ function calculateSalary(baseSalary, bonus, taxRate) {
 }
 console.log(calculateSalary(5000, 500, 0.1)); // Net Salary: $5000
 console.log(calculateSalary(7000, 1000, 0.15)); // Net Salary: $6950
+
+// Task 2
+function calculateDiscount(price, discountRate) {
+    let finalPrice = price - (price * discountRate);
+    return `Final Price: $${finalPrice.toFixed(2)}`;
+}
+console.log(calculateDiscount(100, 0.2)); // Final Price: $80.00
+console.log(calculateDiscount(250, 0.15)); // Final Price: $212.50
